ffb24c66341e507d398302aa2e5476063f194631
ZeinabJahanbakhsh <jahanbakhsh.zeinab@gmail.com>
2024-04-24 22:34:14 +0330

    add ChangeStatusController and Exception:render()

--- app/Http/Controllers/Task/ChangeStatusController.php
+++ app/Http/Controllers/Task/ChangeStatusController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Task;
 use App\Http\Controllers\Controller;
 use App\Models\Status;
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ChangeStatusController extends Controller
 {
-    public function changeStatus(Task $task, Request $request)
+    public function changeStatus(Task $task, Request $request): JsonResponse
     {
         $status = Status::findOrFail($request->integer('status_id'));
 
